a4c1751793650c2d5a2bb932e6fb3fb4c3211e95
Renan Marcelino <renanmarcelinodass@gmail.com>
2024-08-29 21:34:11 -0300

    Add files via upload

---
+++ aula02/scrypt.js
@@ -0,0 +1,12 @@
+document.write("ALUGUEL DE CARROS<BR>");
+document.write("<br>1 - estoque um carro <br>");
+let op = window.prompt("escolha a opção desejada:");
+switch(op) {
+    case "1":
+    document.write("mobi - r$ 150");
+    document.write("<br>corola - R$ 200");
+    document.write("<br>onix - R$ 300");
+break;
+default:
+    document.write("obrigado volte sempre");
+}
---
+++ ex03/dowhile/scrypt.js
@@ -0,0 +1,6 @@
+let i = 0;
+
+do {
+    document.write("o valor de i e: " + i + "<br>");
+    i++;
+}while(i<5 );
